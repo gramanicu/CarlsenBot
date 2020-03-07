f28f3a922bb89ee8adc45a06c1f6942d0a09ade9
gramanicu <gramanicu@gmail.com>
2020-03-07 19:17:36 +0200

    Added unit test and expanded the point functionality

--- src/com/carlsenbot/pieces/Bishop.java
+++ src/com/carlsenbot/pieces/Bishop.java
@@ -3,7 +3,6 @@ package com.carlsenbot.pieces;
 import com.carlsenbot.position.Position;
 
 public class Bishop extends Piece {
-
     public Bishop(boolean isWhite, Position position) {
         super(3.25d, isWhite, position);
     }
