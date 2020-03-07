404526fa9b6fda5152503e7753455e1e48ce7276
gramanicu <gramanicu@gmail.com>
2020-03-07 16:08:13 +0200

    Started working on table

---
+++ src/com/carlsenbot/position/Position.java
@@ -0,0 +1,6 @@
+package com.carlsenbot.position;
+
+public class Position {
+    private int x;
+    private int y;
+}
--- src/com/carlsenbot/table/Table.java
+++ src/com/carlsenbot/table/Table.java
@@ -1,5 +1,11 @@
 package com.carlsenbot.table;
 
 public class Table {
-    
+    private byte[][] matrix;
+
+    public Table() {
+        matrix = new byte[8][8];
+    }
+
+
 }
