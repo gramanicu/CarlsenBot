a9ae518bb019f0353d9c5847925a24629dd47c60
vladmosessohn <vlad_mose@yahoo.com>
2020-03-09 00:40:00 +0200

    method isValidMove optimized in class Pawn, but still not working because of coordinates

--- src/com/carlsenbot/main/Main.java
+++ src/com/carlsenbot/main/Main.java
@@ -1,12 +1,21 @@
 package com.carlsenbot.main;
 
+import com.carlsenbot.pieces.Pawn;
+import com.carlsenbot.position.Position;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         Game g = new Game();
         g.initializeGame();
+        System.out.println();
+
+
 //        System.out.println(g.printPiecesPositions());
 //        System.out.println(g.getTable());
         System.out.println(g.printTable());
         g.startGame();
+
     }
 }
--- src/com/carlsenbot/pieces/Pawn.java
+++ src/com/carlsenbot/pieces/Pawn.java
@@ -19,70 +19,70 @@ public class Pawn extends Piece {
         this.firstMove = firstMove;
     }
 
-    public boolean isValidMove(Table table, int currentRow, int currentCol, int newRow, int newCol) {
+    public boolean isValidMove(Table table, Position currentPos, Position newPos) {
         if (isWhite()) {
-            if (currentRow > newRow) {
+            if (currentPos.getX() > newPos.getX()) {
                 return false;
             }
         } else {
-            if (currentRow < newRow) {
+
+            if (currentPos.getX() < newPos.getX()) {
                 return false;
             }
         }
-
-        if (currentCol == newCol) {
+        System.out.println(currentPos.getY());
+        System.out.println(currentPos.getX());
+        System.out.println(newPos.getY());
+        if (currentPos.getY() == newPos.getY()) {
             //Not taking a piece
             if (isWhite()) {
-                if (table.getPositions()[currentRow + 1][currentCol] != 0) {
+                if (table.getPositions()[currentPos.getX() + 1][currentPos.getY()] != 0) {
                     return false;
                 }
             } else {
-                if (table.getPositions()[currentRow - 1][currentCol] != 0) {
+                if (table.getPositions()[currentPos.getX() - 1][currentPos.getY()] != 0) {
                     return false;
                 }
             }
 
-            if (Math.abs(currentCol - newCol) > 2) {
+            if (Math.abs(currentPos.getX() - newPos.getX()) > 2) {
                 return false;
             }
 
-            if (Math.abs(currentCol - newCol) == 2) {
+            if (Math.abs(currentPos.getX() - newPos.getX()) == 2) {
                 if (firstMove) {
                     return false;
                 }
                 if (isWhite()) {
-                    if (table.getPositions()[currentRow + 2][currentCol] != 0) {
+                    if (table.getPositions()[currentPos.getX() + 2][currentPos.getY()] != 0) {
                         return false;
                     }
                 } else {
-                    if (table.getPositions()[currentRow - 2][currentCol] != 0) {
+                    if (table.getPositions()[currentPos.getX() - 2][currentPos.getY()] != 0) {
                         return false;
                     }
                 }
             }
         } else {
-            if (Math.abs(newCol - currentCol) != 1) {
-                return false;
-            }
-            if (Math.abs(newRow - currentRow) != 1) {
+            if (Math.abs(newPos.getY() - currentPos.getY()) != 1 || Math.abs(newPos.getX() - currentPos.getX()) != 1) {
                 return false;
             }
         }
 
-        if (table.getPositions()[newRow][newCol] != 0) {
+        if (table.getPositions()[newPos.getX()][newPos.getY()] != 0) {
             return false;
         }
 
-        if (newCol < 0 || newCol > 7) {
+        if (newPos.getY() < 0 || newPos.getY() > 7) {
             return false;
         }
 
         if(isWhite()) {
-            if(newRow < 0) {
+            if(newPos.getX() < 0) {
                 return false;
             }
         } else {
-            if(newRow > 7) {
+            if(newPos.getX() > 7) {
                 return false;
             }
         }
@@ -100,7 +100,7 @@ public class Pawn extends Piece {
 
     @Override
     public boolean move(Position target, Table table) {
-        if (isValidMove(table, getPosition().getY(), getPosition().getX(), target.getX(), target.getY())) {
+        if (isValidMove(table, getPosition(), target)) {
             super.setPosition(target);
             return true;
         }
