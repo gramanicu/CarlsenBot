7ae2515110292e02ccc360bbc6d03d92b250e996
WansE1z <radu.ionita0702@gmail.com>
2020-03-12 09:39:11 +0200

    Added bishop test, soon to be finished

---
+++ test/com/carlsenbot/pieces/BishopTest.java
@@ -0,0 +1,45 @@
+/*
+ * © 2020 Grama Nicolae, Ioniță Radu , Mosessohn Vlad, 322CA
+ */
+
+package com.carlsenbot.pieces;
+
+import com.carlsenbot.position.Position;
+import com.carlsenbot.table.Table;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopTest {
+    Bishop b1 = new Bishop(PieceColor.White, "a2");
+    Bishop b2 = new Bishop(PieceColor.White, "a3");
+    Bishop b3 = new Bishop(PieceColor.White, "a4");
+    Bishop b4 = new Bishop(PieceColor.Black, "b3");
+    Bishop b5 = new Bishop(PieceColor.Black, "b4");
+    Bishop b6 = new Bishop(PieceColor.White, "h1");
+    Table table;
+
+    @Test
+    void isValidMove() {
+        table = new Table();
+        table.addPiece(b1);
+        table.addPiece(b2);
+        table.addPiece(b3);
+        table.addPiece(b4);
+        table.addPiece(b5);
+        table.addPiece(b6);
+
+        assertFalse(b1.isValidMove(new Position("a3")).canMove, "The bishop should not be able to move");
+        assertFalse(b1.isValidMove(new Position("a4")).canMove, "The bishop should not be able to move");
+        assertFalse(b2.isValidMove(new Position("a4")).canMove, "The bishop should not be able to move");
+
+        assertFalse(b3.isValidMove(new Position("a5")).canMove, "The bishop should not be able to move there");
+        assertFalse(b3.isValidMove(new Position("a6")).canMove, "The bishop should not be able to move there");
+
+        //assertTrue(b6.isValidMove(new Position("a8")).canMove, "The bishop should pass the whole board on diagonal");
+    }
+
+    @Test
+    void move() {
+    }
+}
