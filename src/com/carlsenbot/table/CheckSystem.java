118be041b7a03fa6adb1d714c1fc9591d77b1044
gramanicu <gramanicu@gmail.com>
2020-03-17 13:45:24 +0200

    Simplified checksystem
    Removed unnecessary move
    Solved checksystem bug

--- src/com/carlsenbot/table/CheckSystem.java
+++ src/com/carlsenbot/table/CheckSystem.java
@@ -10,44 +10,25 @@ import com.carlsenbot.pieces.PieceColor;
 import com.carlsenbot.position.Position;
 
 public class CheckSystem {
-    private byte[][] dangerW;
-    private byte[][] dangerB;
+    public CheckSystem() { }
 
-
-    /**
-     * Initialise an empty table
-     */
-    public CheckSystem() {
-        dangerW = new byte[8][8];
-        dangerB = new byte[8][8];
-    }
-
-    /**
-     * Recompute the check table for the white player (who he is attacked by)
-     * @param color The player
-     */
-    public void computeCheck(PieceColor color) {
-
-        if(color == PieceColor.White) {
-            dangerW = new byte[8][8];
-        } else {
-            dangerB = new byte[8][8];
-        }
-
-    }
 
     public boolean isInCheck(PieceColor playerColor) {
         Table table = GameManager.getInstance().getTable();
         Piece[] pieces;
         Position kingPos;
-        if(playerColor.equals("White")) {
+        if(playerColor == PieceColor.White) {
             pieces = table.getPieces()[1];
             kingPos = table.getPieces()[0][0].getPosition();
         } else {
             pieces = table.getPieces()[0];
             kingPos = table.getPieces()[1][0].getPosition();
         }
+
         for(Piece p : pieces) {
+            if (p == null) {
+                continue;
+            }
             if(p.isValidMove(kingPos).attacking) {
                 return true;
             }
